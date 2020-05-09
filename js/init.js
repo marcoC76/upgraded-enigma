b8ac7955fa6e05de75379e938b0a173fed3746fd
Marco Antonio <marco.perez.cetis76@gmail.com>
2020-05-09 15:01:26 -0500

    nuevo

--- js/init.js
+++ js/init.js
@@ -1,23 +1,34 @@
-var ks, kx, ky;
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+
+
+function m(a) {
+  var ks, kx, ky;
+var numero=a;
 
 //random image on page load
 var images = ['https://s3-us-west-2.amazonaws.com/s.cdpn.io/131045/ksImg.jpg',
-              'https://s3-us-west-2.amazonaws.com/s.cdpn.io/131045/Colorfuleye.jpg',
-              'https://s3-us-west-2.amazonaws.com/s.cdpn.io/131045/geometric.jpg',
-              'https://s3-us-west-2.amazonaws.com/s.cdpn.io/131045/bwSpiral.jpg',
-              'https://s3-us-west-2.amazonaws.com/s.cdpn.io/131045/colorfulVertex.jpg',
-              'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRDf8CV424Sjy9JRmsZc2yZ5BjYCKjUO-ZLFzs2plkovbQHpnsZ'
-           ];
+  'https://s3-us-west-2.amazonaws.com/s.cdpn.io/131045/Colorfuleye.jpg',
+  /* 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/131045/geometric.jpg', */
+  /* 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/131045/bwSpiral.jpg', */
+  'https://s3-us-west-2.amazonaws.com/s.cdpn.io/131045/colorfulVertex.jpg',
+  'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRDf8CV424Sjy9JRmsZc2yZ5BjYCKjUO-ZLFzs2plkovbQHpnsZ'
+];
 
-var rndImg = function() {
+var rndImg = function () {
   var len = images.length;
-  
-  var rnd = Math.floor(Math.random() * len);
-  return images[3];
+
+  rnd = Math.floor(Math.random() * len);
+  return images[numero];
 }
 
-var Set = function() {};
-Set.partitions = function(display, img, scale) {
+var Set = function () {};
+Set.partitions = function (display, img, scale) {
   var c1 = document.createElement('canvas');
   var c2 = document.createElement('canvas');
   var w = c1.width = c2.width = img.naturalWidth || img.width;
@@ -25,13 +36,13 @@ Set.partitions = function(display, img, scale) {
   var $ = c1.getContext('2d');
   var $$ = c2.getContext('2d');
   $.drawImage(img, 0, 0);
-  
+
   while (scale < 0.3) {
     $$.clearRect(0, 0, w / 2, h / 2);
     $$.drawImage(c1, 0, 0, w, h, 0, 0, w / 2, h / 2);
-    w /= 2;
-    h /= 2;
-    scale *= 2;
+    w /= 3;
+    h /= 3;
+    scale *= 3;
     var hiddencanvas = c1;
     c1 = c2;
     c2 = hiddencanvas;
@@ -46,12 +57,12 @@ Set.partitions = function(display, img, scale) {
 
 var opts = {
   imgURL: rndImg(),
-  speed: 0.002,
-  segmentSize: 200,
+  speed: 0.003,
+  segmentSize: 100,
   smoothing: 0.1,
 };
 
-var Obj = function(canvas, segmentMotion) {
+var Obj = function (canvas, segmentMotion) {
   this._c = canvas;
   this._$ = canvas.getContext('2d');
   this.segmentWidth = segmentMotion;
@@ -69,7 +80,7 @@ var Obj = function(canvas, segmentMotion) {
 };
 
 Obj.prototype = {
-  draw: function() {
+  draw: function () {
     var _$_ = this.stockctx;
     var w = this.segmentWidth;
     var h = this.segmentHeight;
@@ -100,12 +111,12 @@ Obj.prototype = {
     }
   },
 
-  resize: function(w, h) {
+  resize: function (w, h) {
     this._c.width = w;
     this._c.height = h;
   },
 
-  setImage: function(img) {
+  setImage: function (img) {
     var calibrate = Math.max(this.segmentWidth / img.naturalWidth, this.segmentHeight / img.naturalHeight);
 
     if (calibrate < 1.0) {
@@ -117,7 +128,7 @@ Obj.prototype = {
     }
   },
 
-  drawSegment: function(_$_, kx, ky, dt, reverse) {
+  drawSegment: function (_$_, kx, ky, dt, reverse) {
     var w = this.segmentWidth;
     var h = this.segmentHeight;
 
@@ -142,66 +153,66 @@ Obj.prototype = {
 };
 
 function ready() {
-    var c = document.getElementById('canv');
-    ks = new Obj(c, opts.segmentSize);
-    ks.resize(window.innerWidth, window.innerHeight);
-    kx = 0;
-    ky = 0;
+  var c = document.getElementById('canv');
+  ks = new Obj(c, opts.segmentSize);
+  ks.resize(window.innerWidth, window.innerHeight);
+  kx = 0;
+  ky = 0;
 
   loadImage(opts.imgURL, setImage);
 
-  window.addEventListener('mousemove', function(e) {
-      kx = (e.clientX / window.innerWidth - 0.5) * opts.segmentSize;
-      ky = (e.clientY / window.innerHeight - 0.5) * opts.segmentSize;
+  window.addEventListener('mousemove', function (e) {
+    kx = (e.clientX / window.innerWidth - 0.5) * opts.segmentSize;
+    ky = (e.clientY / window.innerHeight - 0.5) * opts.segmentSize;
   });
 
-  window.addEventListener('touchmove', function(e) {
-      e.preventDefault();
-      kx = (e.touches[0].clientX / window.innerWidth - 0.5) * opts.segmentSize;
-      ky = (e.touches[0].clientY / window.innerHeight - 0.5) * opts.segmentSize;
+  window.addEventListener('touchmove', function (e) {
+    e.preventDefault();
+    kx = (e.touches[0].clientX / window.innerWidth - 0.5) * opts.segmentSize;
+    ky = (e.touches[0].clientY / window.innerHeight - 0.5) * opts.segmentSize;
   });
 
-  
-  window.addEventListener('dragover', function(e) {
-      e.preventDefault();
+
+  window.addEventListener('dragover', function (e) {
+    e.preventDefault();
   });
 
-  window.addEventListener('drop', function(e) {
-      e.preventDefault();
-      if (e.dataTransfer.files.length < 1) {
-        return;
-      }
-      openFile(e.dataTransfer.files[0], setImage);
+  window.addEventListener('drop', function (e) {
+    e.preventDefault();
+    if (e.dataTransfer.files.length < 1) {
+      return;
+    }
+    openFile(e.dataTransfer.files[0], setImage);
   });
 
-  window.addEventListener("paste", function(e){
-      if(e.clipboardData == false) return false; 
-      var items = e.clipboardData.items;
-      if(items == undefined) return false;
+  window.addEventListener("paste", function (e) {
+    if (e.clipboardData == false) return false;
+    var items = e.clipboardData.items;
+    if (items == undefined) return false;
 
-      for (var i = 0; i < items.length; i++) {
-        if (items[i].type.indexOf("image") == -1) continue; 
-        var item = items[i].getAsFile();
-        var URL = window.URL || window.webkitURL;
-        var src = URL.createObjectURL(item);
-      }
-      copyPasteImage(src, setImage);
+    for (var i = 0; i < items.length; i++) {
+      if (items[i].type.indexOf("image") == -1) continue;
+      var item = items[i].getAsFile();
+      var URL = window.URL || window.webkitURL;
+      var src = URL.createObjectURL(item);
+    }
+    copyPasteImage(src, setImage);
   });
 
-  window.addEventListener('resize',function(){
-      c1.width = c2.width = w = src.naturalWidth || src.width;
-      c1.height = c2.height = h = src.naturalHeight || src.height;
+  window.addEventListener('resize', function () {
+    c1.width = c2.width = w = src.naturalWidth || src.width;
+    c1.height = c2.height = h = src.naturalHeight || src.height;
   });
 
-  window.addEventListener('deviceorientation', function(e) {
-      kx = opts.segmentSize * Math.sin(e.beta * Math.PI / 180);
-      ky = opts.segmentSize * Math.sin(e.gamma * Math.PI / 90);
+  window.addEventListener('deviceorientation', function (e) {
+    kx = opts.segmentSize * Math.sin(e.beta * Math.PI / 180);
+    ky = opts.segmentSize * Math.sin(e.gamma * Math.PI / 90);
   });
 
-  setTimeout(function() {
-      window.addEventListener('resize', function(e) {
-        ks.resize(window.innerWidth, window.innerHeight);
-      });
+  setTimeout(function () {
+    window.addEventListener('resize', function (e) {
+      ks.resize(window.innerWidth, window.innerHeight);
+    });
   }, 3000);
 }
 
@@ -221,18 +232,18 @@ function openFile(file, callback) {
   loadImage(URL.createObjectURL(file), callback);
 }
 
- function copyPasteImage(src, callback){
-   var img = new Image();
-   img.onload = function(e){
-     callback(e.target);
-   }
-	 img.src = src;
+function copyPasteImage(src, callback) {
+  var img = new Image();
+  img.onload = function (e) {
+    callback(e.target);
+  }
+  img.src = src;
 }
 
 function loadImage(src, callback) {
   var img = new Image();
-  img.onload = function(e) {
-   callback(e.target);
+  img.onload = function (e) {
+    callback(e.target);
   };
   img.src = src;
 }
@@ -245,3 +256,4 @@ ready();
 draw();
 
 console.log('Code With ❤ Always, @tmrDevelops');
+}
